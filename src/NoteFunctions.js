a0919904eac1ccf6c1ccadf4e2f2d4ba243cb69d
A Hashim <ahashim983@yahoo.com>
2020-02-20 13:17:57 +0000

    foundational styling finished, starting note functionality

---
+++ src/NoteFunctions.js
@@ -0,0 +1,6 @@
+export default class NoteFunctions {
+    constructor({ note, notes }){
+        this.note = note
+        this.notes = notes
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-import {Person} from './Person';
 import './scss/style.scss';
+import NoteFunctions from "./NoteFunctions"
 
-const p = new Person('Zura');
-console.info(p.sayHello());
+note = document.querySelector('#notesContainer')
+notes = [{
+    title: 'hello',
+    content: 'how are you'
+},
+{
+    title: 'abc',
+    content: 'def'
+}]
+const NoteFunctions = new NoteFunctions()
